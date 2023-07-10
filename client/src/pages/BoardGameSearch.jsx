b8404ab0ed59edfd48e1e72c26e75b49c58ae8fd
Figaro <luis.figaro@gmail.com>
2023-07-10 01:31:44 -0300

    removing unused references

--- client/src/pages/BoardGameSearch.jsx
+++ client/src/pages/BoardGameSearch.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
-import ReactTable from "react-table-6";
 import api from '../api'
 
 import styled from 'styled-components'
-
-import "react-table-6/react-table.css" 
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
@@ -65,16 +62,7 @@ class BoardGameList extends Component {
 
         return (
             <Wrapper>
-                {showTable && (
-                    <ReactTable
-                        data={boardgames}
-                        columns={columns}
-                        loading={isLoading}
-                        defaultPageSize={10}
-                        showPageSizeOptions={true}
-                        minRows={0}
-                    />
-                )}
+
             </Wrapper>
         )
     }
