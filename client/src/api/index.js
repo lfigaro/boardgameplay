6dcd13ea86a235165f6abe7a21697d44cbb6aa6b
Figaro <luis.figaro@gmail.com>
2023-07-10 01:56:59 -0300

    resolving build issues

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.REACT_APP_CLIENT_URL,
 })
 
 export const getBoardGameList = () => api.get(`/boardgame/list`)
